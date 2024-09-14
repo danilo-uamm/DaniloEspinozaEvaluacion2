e3d8e23d326c5940f7e06a1a9bece651e2e87863
Carlos Fernando Roque Urbina <croqueu1907@outlook.com>
2024-09-14 16:15:30 -0600

    Este programa ha sido realizado con Notacion Hungara y CamelCase

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ namespace DaniloEspinozaEvaluacion2
     {
 
         private List<Model.Persona> lstPersonas = new List<Model.Persona>();
+
         int iEdad;
 
         private Persona PersonaNueva = new Persona();
